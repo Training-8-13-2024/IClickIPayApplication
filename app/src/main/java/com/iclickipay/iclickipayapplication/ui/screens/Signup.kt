f2110d1f6b698ebd19641a684d8c981c2b2d2497
adenikinju daniel <adenikinjudaniel@gmail.com>
2024-10-04 18:19:52 -0400

    sign up screen created

--- app/src/main/java/com/iclickipay/iclickipayapplication/ui/screens/Signup.kt
+++ app/src/main/java/com/iclickipay/iclickipayapplication/ui/screens/Signup.kt
@@ -1,7 +1,6 @@
 package com.iclickipay.iclickipayapplication.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,8 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
